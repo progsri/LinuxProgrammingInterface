3426e0c329b5592a3b76c469c94eb7cb61c3bf09
srikanth <srikanth.83sys@gmail.com>
2018-11-21 13:14:37 -0600

    using proper headers

--- File_IO/open_file_if_not_present.c
+++ File_IO/open_file_if_not_present.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <fcntl.h> 
+#include <sys/stat.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     printf("Looking for file \n ");
 
-    char fileName = "./temp";
-    int fd = open(fileName, "O_WRONLY");
-    if(fd != -1){
+    char *fileName = "temp";
+    int fd = open(fileName, O_RDONLY | O_CREAT);
+
+    printf(" File descriptor %d \n ", fd);
+    if (fd != -1)
+    {
         printf("Success \n ");
-    }else{
-        printf("File might exists \n ");
+    }
+    else
+    {
+        printf("File might exists or ERROR \n ");
     }
 }
